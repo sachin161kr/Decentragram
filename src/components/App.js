a776af576f6144b03e7b1bd19bec48c81d70a1c3
Sachin Kumar <mail.sachin161@gmail.com>
2022-10-04 18:13:37 +0530

    Decentragram Stage 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,155 @@
-import React, { Component } from 'react';
-import Web3 from 'web3';
-import Identicon from 'identicon.js';
-import './App.css';
-import Decentragram from '../abis/Decentragram.json'
-import Navbar from './Navbar'
-import Main from './Main'
+import React, { Component } from "react";
+import Web3 from "web3";
+import Identicon from "identicon.js";
+import "./App.css";
+import Decentragram from "../abis/Decentragram.json";
+import Navbar from "./Navbar";
+import Main from "./Main";
 
+const ipfsClient = require("ipfs-http-client");
+
+const projectId = "2FfUNn5e60AWhBaRNFK1IiY9JrA";
+
+const projectSecret = "b67440bad4792e4a6b4d0a2ec114bbe5";
+
+const auth =
+  "Basic " + Buffer.from(projectId + ":" + projectSecret).toString("base64");
+
+const ipfs = ipfsClient({
+  host: "ipfs.infura.io",
+  port: 5001,
+  protocol: "https",
+  headers: {
+    authorization: auth,
+  },
+});
 
 class App extends Component {
+  async componentWillMount() {
+    this.loadWeb3();
+    this.loadBlockChainData();
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+  }
+
+  async loadBlockChainData() {
+    const web3 = window.web3;
+    // load acc
+    const accounts = await web3.eth.getAccounts();
+    //console.log(accounts[0]);
+    this.setState({ account: accounts[0] });
+
+    // Network id
+
+    const networkId = await web3.eth.net.getId();
+    const networkData = await Decentragram.networks[networkId];
+
+    if (networkData) {
+      const decentragram = web3.eth.Contract(
+        Decentragram.abi,
+        networkData.address
+      );
+      this.setState({ decentragram });
+      const imagesCount = await decentragram.methods.imageCount().call();
+      this.setState({ imagesCount });
+
+      for (var i = 1; i <= imagesCount; i++) {
+        const image = await decentragram.methods.images(i).call();
+        this.setState({
+          images: [...this.state.images, image],
+        });
+      }
+
+      this.setState({
+        images: this.state.images.sort((a, b) => b.tipAmount - a.tipAmount),
+      });
+
+      this.setState({ loading: false });
+    } else {
+      window.alert("Decentragram contract not deployed to detected network");
+    }
+  }
+
+  captureFile = (event) => {
+    event.preventDefault();
+    const file = event.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
+
+    reader.onloadend = () => {
+      this.setState({ buffer: Buffer(reader.result) });
+      console.log("buffer", this.state.buffer);
+    };
+  };
+
+  uploadImage = (description) => {
+    console.log("Submitting file to ipfs...");
+
+    ipfs.add(this.state.buffer, (error, result) => {
+      console.log("Ipfs result", result);
+
+      if (error) {
+        console.log(error);
+        return;
+      }
+
+      this.setState({ loading: true });
+      this.state.decentragram.methods
+        .uploadImage(result[0].hash, description)
+        .send({ from: this.state.account })
+        .on("transactionHash", (hash) => {
+          this.setState({ loading: false });
+        });
+    });
+  };
+
+  tipImageOwner = (id, tipAmount) => {
+    this.setState({ loading: true });
+    this.state.decentragram.methods
+      .tipImageOwner(id)
+      .send({ from: this.state.account, value: tipAmount })
+      .on("transactionHash", (hash) => {
+        this.setState({ loading: false });
+      });
+  };
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      account: '',
-    }
+      account: "",
+      decentragram: null,
+      images: [],
+      loading: true,
+    };
   }
 
   render() {
     return (
       <div>
         <Navbar account={this.state.account} />
-        { this.state.loading
-          ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
-          : <Main
-            // Code...
-            />
-          }
-        }
+        {this.state.loading ? (
+          <div id="loader" className="text-center mt-5">
+            <p>Loading...</p>
+          </div>
+        ) : (
+          <Main
+            images={this.state.images}
+            captureFile={this.captureFile}
+            uploadImage={this.uploadImage}
+            tipImageOwner={this.tipImageOwner}
+          />
+        )}
       </div>
     );
   }
